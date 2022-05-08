2a2f78f9c69048ac036e57a6cbf2abfd851331eb
Helen Hu <helenhu@usc.edu>
2022-05-07 21:59:21 -0700

    modified constructors

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -6,14 +6,18 @@ public class Company {
 	public String name;
 	
 	public int numApps;
+	
+	public Company(String companyID, String companyName, int numApps) {
+		// TODO Auto-generated constructor stub
+		this.id = companyID;
+		this.name = name;
+		this.numApps = numApps;
+	}
+
 	public Company() {
-		
+		// TODO Auto-generated constructor stub
 	}
-	public Company(String mid, String mname, int mnumApps) {
-		id = mid;
-		name = mname;
-		numApps = mnumApps;
-	}
+
 	public String getId() {
 		return id;
 	}
